1080e34c02dada49e45285d4eacdf5668cca120c
QuantumLeap <40482494+QuantumLeap-us@users.noreply.github.com>
2023-12-08 01:45:00 +0800

    Update Media.tsx

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,32 +1,30 @@
-import { Box } from "@mui/material";
-import Link from "next/link";
+interface MediaItem {
+  title: string;
+  linkText: string;
+  link: string;
+}
 
 export default function Media() {
-  const mediaList = [];
+  const mediaList: MediaItem[] = [];
 
   return (
-    <div className=" py-4">
-      <div className=" flex items-center justify-center gap-x-4 max-sm:flex-col">
-        {mediaList.map(({ title, linkText, link }) => {
-          return (
-            <div
-              key={title}
-              className=" flex items-center gap-2 text-xl"
+    <div className="py-4">
+      <div className="flex items-center justify-center gap-x-4 max-sm:flex-col">
+        {mediaList.map(({ title, linkText, link }) => (
+          <div key={title} className="flex items-center gap-2 text-xl">
+            <span>{title}:</span>
+            <Box
+              component={Link}
+              href={link}
+              className="hover:underline"
+              sx={{
+                color: "primary.main",
+              }}
             >
-              <span>{title}:</span>
-              <Box
-                component={Link}
-                href={link}
-                className=" hover:underline"
-                sx={{
-                  color: "primary.main",
-                }}
-              >
-                {linkText}
-              </Box>
-            </div>
-          );
-        })}
+              {linkText}
+            </Box>
+          </div>
+        ))}
       </div>
     </div>
   );
